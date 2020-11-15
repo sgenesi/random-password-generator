35fa19cbffd6b42a55388b969cc94133f515271c
Staci Genesi <sgenesi@hotmail.com>
2020-11-15 11:43:27 -0700

    character values

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
 
+// Special characters values
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "_", "`", "{", "|", "}", "~"];
+
+// Numeric character values
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Alphabet characters
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
