0c647655e581472dd58f4f824e9191e3eefb1fe7
Staci Genesi <sgenesi@hotmail.com>
2020-11-15 11:50:37 -0700

    add uppercase conversion

--- script.js
+++ script.js
@@ -15,6 +15,15 @@ number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Alphabet characters
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// Uppercase conversion
+space = [];
+
+var toUpper = function (x) {
+  return x.toUppercase();
+};
+
+alpha2 = alpha.map(toUpper);
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
