c86b53238f86af0c1e77876e0b3fb182e153ea55
Staci Genesi <sgenesi@hotmail.com>
2020-11-15 12:17:43 -0700

    rework variable and arrays

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
 // Assignment code here
-// Use input variables
-var enter;
-var confirmNumber;
-var confirmCharacter;
+// User input variables
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
 var confirmUppercase;
 var confirmLowercase;
 
-// Special characters values
-character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "_", "`", "{", "|", "}", "~"];
-
-// Numeric character values
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// Alphabet characters
-alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-// Uppercase conversion
-space = [];
-
-var toUpper = function (x) {
-  return x.toUppercase();
-};
-
-alpha2 = alpha.map(toUpper);
+// Arrays
+var specialCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "_", "`", "{", "|", "}", "~"];
+var numbericCharacter = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var alphaLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
