278ec542945d050b249dbd2ee4f736db05d34aaa
Staci Genesi <sgenesi@hotmail.com>
2020-11-15 14:40:44 -0700

    fix spelling errors to correct generate issue

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Assignment code here
+
 // User input variables
 var confirmLength = "";
 var confirmSpecialCharacter;
@@ -8,11 +9,11 @@ var confirmLowercase;
 
 // Arrays
 var specialCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "_", "`", "{", "|", "}", "~"];
-var numbericCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numericCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var alphaLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Prompt to confirm how many characters 
+// Prompt to confirm # of characters 
 function generatePassword() {
   var confirmLength = (prompt("How many characters should your password contain?"));
 
@@ -37,28 +38,28 @@ function generatePassword() {
         var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");   
     } 
 
-    // Assign an action to the password parameters NEED TO FIX THIS
+    // Assign action to password parameters 
     var passwordCharacters = []
       
     if (confirmSpecialCharacter) {
-      passwordCharacters = passwordCharacters.concat(specialChar)
+      passwordCharacters = passwordCharacters.concat(specialCharacter)
     }
 
     if (confirmNumericCharacter) {
-      passwordCharacters = passwordCharacters.concat(number)
+      passwordCharacters = passwordCharacters.concat(numericCharacter)
     }
       
     if (confirmLowerCase) {
-      passwordCharacters = passwordCharacters.concat(alphaLower)
+      passwordCharacters = passwordCharacters.concat(alphaLowerCase)
     }
 
     if (confirmUpperCase) {
-      passwordCharacters = passwordCharacters.concat(alphaUpper)
+      passwordCharacters = passwordCharacters.concat(alphaUpperCase)
     }
 
       console.log(passwordCharacters)
 
-      // Empty string to be filled based on for loop selecting random characters from the array
+      // Empty string to be filled based on for loop selection 
       var randomPassword = ""
       
       for (var i = 0; i < confirmLength; i++) {
@@ -77,7 +78,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
